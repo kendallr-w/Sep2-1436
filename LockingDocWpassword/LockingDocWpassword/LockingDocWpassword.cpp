3e51020becce1ccef7560b374331dc8df1a6f1f4
Kendall Reed-Warren <reedwarrenkj@gmail.com>
2025-10-16 10:24:51 -0500

    locking a doc w/ password assignment unfinished. view github to finish.

---
+++ LockingDocWpassword/LockingDocWpassword/LockingDocWpassword.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include <fileReadingFunctions.h>
+
+int main()
+{
+	std::cout << "Enter a password (for locking a file)\n";
+	std::string correctPassword;
+	std::getline(std::cin, correctPassword);
+
+	std::cout << "Does thou wish to view thy document(yes/no)\n";
+	std::string yesOrNo;
+	std::getline(std::cin, yesOrNo);
+
+	if (yesOrNo == "yes")
+	{
+		std::string passwordAttempt = promptForPassword();
+
+		if (passwordAttempt == correctPassword)
+
+		{
+			displayContentsOfFile(filename);
+		}
+	}
+		
+
---
+++ LockingDocWpassword/LockingDocWpassword/fileReadingFunctions.cpp
@@ -0,0 +1,31 @@
+#include "fileReadingFunctions.h"
+#include <iostream>
+
+#include <string>
+
+std::string promptForPassword()
+{
+	std::cout << "Enter the password that you set earlier:\n";
+	std::string passwordAttempt;
+	std::getline(std::cin, passwordAttempt);
+	return passwordAttempt;
+}
+
+void displayContentsOfFile(std::string filename)
+{
+	std::ifstream fin("secretFile.txt");
+
+	if (!fin.is_open())
+	{
+		std::cout << "Unable to find file\n";
+
+		return;
+	}
+
+	std::string currentLineInFile;
+	while (std::getline(fin, currentLineInFile))
+	{
+		std::cout << currentLineInFile << '\n';
+	}
+	
+}
